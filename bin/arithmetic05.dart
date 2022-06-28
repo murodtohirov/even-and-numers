138d92413ba808a7a4e853718232edeaa27f35a8
Пользователь <murodjontohirov2701@gmail.com>
2022-06-28 14:50:32 +0500

    arithmetic05

--- bin/arithmetic05.dart
+++ bin/arithmetic05.dart
@@ -1,4 +1,13 @@
 void main() {
-  int x = 89;
-  print();
+  int x = 8521;
+  int x1 = x % 10;
+  int x2 = (x % 100) ~/ 10;
+  int x3 = (x ~/ 100) % 10;
+  int x4 = (x % 100) ~/ 10;
+  int a = x1 % 10;
+  int b = x2 % 10;
+  int c = x3 % 10;
+  int d = x4 % 10;
+
+  print(x4);
 }
