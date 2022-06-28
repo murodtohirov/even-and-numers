2d7301e7cc166dc70bd48ab2593e2fc9c77868fd
Пользователь <murodjontohirov2701@gmail.com>
2022-06-28 15:40:38 +0500

    arithmetic07

--- bin/arithmetic05.dart
+++ bin/arithmetic05.dart
@@ -3,11 +3,12 @@ void main() {
   int x1 = x % 10;
   int x2 = (x % 100) ~/ 10;
   int x3 = (x ~/ 100) % 10;
-  int x4 = (x % 100) ~/ 10;
+  int x4 = (x ~/ 100) ~/ 10;
   int a = x1 % 10;
   int b = x2 % 10;
   int c = x3 % 10;
   int d = x4 % 10;
+  int var_int = a + b + c + d;
 
-  print(x4);
+  print(var_int);
 }
---
+++ bin/arithmetic07.dart
@@ -0,0 +1,12 @@
+void main() {
+  int x = 0;
+  if (x > 0) {
+    print('positive');
+  }
+  if (x < 0) {
+    print('negative');
+  }
+  if (x == 0) {
+    print('zero');
+  }
+}
