7c24f7049d517ae7407f840f59366775d8a93b32
Пользователь <murodjontohirov2701@gmail.com>
2022-06-28 13:43:17 +0500

    arithmetic03

---
+++ bin/arithmetic03.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 6737;
+  int x1 = x % 10;
+  int x2 = (x % 100) ~/ 10;
+  int x3 = (x ~/ 100) % 10;
+  int x4 = (x ~/ 100) ~/ 10;
+  int s = x1 % 2;
+  int h = x2 % 2;
+  int r = x3 % 2;
+  int f = x4 % 2;
+  int var_int = s + h + r + f;
+  print(var_int);
+}
---
+++ bin/arithmetic05.dart
@@ -0,0 +1,4 @@
+void main() {
+  int x = 89;
+  print();
+}
